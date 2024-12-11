553a3625b3f19fdf8d137914b3f53a808830798b
kameeliea <146063731+kameeliea@users.noreply.github.com>
2024-12-11 23:22:51 +0700

    Delete View 10.sql

--- View 10.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW ViewProdukSupplier AS
-SELECT p.Harga, s.No_Kontak, SUM(p.Harga) AS Total_Harga
-FROM Produk p
-JOIN Supplier s ON p.Supplier_ID = s.Supplier_ID
-GROUP BY p.Harga, s.No_Kontak;
-
-SELECT * FROM ViewProdukSupplier;
-
